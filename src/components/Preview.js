b9f035fd7502e8f917d0f1282a41653fa7d0a65b
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2019-05-25 02:08:11 -0700

    Added Markedjs

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,17 @@
 import React from "react";
+import marked from "marked";
 
 const Preview = ({ text }) => {
-  return <div id="preview">{text}</div>;
+  const getMarkdownText = () => {
+    let rawMarkup = marked(text, { sanitize: true });
+    return { __html: rawMarkup };
+  };
+  return (
+    <div>
+      <h5>Previewer</h5>
+      <div id="preview" dangerouslySetInnerHTML={getMarkdownText()} />
+    </div>
+  );
 };
 
 export default Preview;
